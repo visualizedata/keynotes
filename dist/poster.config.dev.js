6dc64f56662b9e312f1988bd661e010142c83697
Daniel Sauter <daniel@ket.ai>
2022-05-04 09:32:56 -0400

    update Jiahui(Jessie) Han

---
+++ dist/poster.config.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  title: 'KEYNOTES',
+  department: 'MS Data Visualization',
+  school: 'Parsons School of Design',
+  location: 'Online',
+  date: '2022-05-18',
+  time: '5:00PM',
+  students: ['Molly Cook', 'Juliet Craig', 'Gisli Gudjonsson', 'Jiahui(Jessie) Han', 'Yann Kerblat', 'Yoana Kosturska', 'Baihan Lin', 'Wenqin Lu', 'Shea Molloy', 'Omar Nema', 'Jeremy Odell', 'Kasey Zhuravlev', 'parsons.nyc']
+};
